ad380d578dbad362a5837eeba42b79ce8d2088bf
lazikaz <souflakey@gmail.com>
2022-12-06 23:22:33 -0700

    ok finished gamelist component for real

--- src/components/game list/GameList.jsx
+++ src/components/game list/GameList.jsx
@@ -2,12 +2,12 @@ import "./GameList.css"
 import { useSearchParams } from "react-router-dom"
 import axios from "axios"
 
-function GameList(){
+function GameList(props){
     //genre, page, 
     let [searchParams, setSearchParams] = useSearchParams()
     console.log(searchParams.get("genre"))
 
-    axios.get(`http://localhost:3001/api/gamesearch?name=${searchParams.get("term")}&location=${searchParams.get("location")}&genre=${searchParams.get("genre")}`)
+    axios.get(`http://localhost:3001/api/gamesearch?name=${searchParams.get("term")}&location=${searchParams.get("location")}&genre=${searchParams.get("genre")}&page=${props.page}`)
 
     return(
         <div className="GameList">
