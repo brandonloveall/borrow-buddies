d3efd805b48af4ae1069411389d515daa07244fd
lazikaz <souflakey@gmail.com>
2022-12-12 23:10:20 -0700

    a

--- src/components/game list/GameList.jsx
+++ src/components/game list/GameList.jsx
@@ -20,15 +20,17 @@ function GameList(){
         <div className="GameList">
             <div className="gameresults">
                 {(games.map((e, i) => {
-                    return (<GameResult color={i % 2 === 0 ? "#A3B18A" : "#588157"} image={e.image} title={e.name} location={e.location} id={e.id}/>)
+                    if(e.id){
+                        return (<GameResult color={i % 2 === 0 ? "#A3B18A" : "#588157"} image={e.image} title={e.name} location={e.location} id={e.id} key={i}/>)
+                    }
                 }))}
             </div>
             <footer className="pagenums">
-                {(() => {
-                    for(let i = 0; i < games.length; i++){
-                        return (<button className="pagenum" key={i} onClick={() => {setSearchParams({page: i + 1}); console.log(searchParams)}}><p>{i + 1}</p></button>)
+                {games.map((e,i) => {
+                    if(i % 10 === 0){
+                        return (<button className="pagenum" key={i} onClick={() => {setSearchParams({page: Math.ceil(i + 1/10)}); console.log(searchParams)}}><p>{Math.ceil(i + 1/10)}</p></button>)
                     }
-                })()}
+                })}
             </footer>
         </div>
     )
--- src/components/game result/GameResult.jsx
+++ src/components/game result/GameResult.jsx
@@ -2,16 +2,18 @@ import "./GameResult.css"
 import { Link } from "react-router-dom"
 import test from "../../assets/gow.jpg"
 
-function GameResult(props){
+function GameResult(props) {
 
 
-    return(
-        <div className="GameResult" style={{backgroundColor: props.color}}>
+    return (
+        <div className="GameResult" style={{ backgroundColor: props.color }}>
             <Link className="gameresultlink" to={`/c/game/${props.id}`}>
-                <img src={props.image} alt="" />
-                <div>
-                    <p className="title">{props.title}</p>
-                    <p className="location">{props.location}</p>
+                <div className="leftside">
+                    <img src={props.image} alt="" />
+                    <div>
+                        <p className="title">{props.title}</p>
+                        <p className="location">{props.location}</p>
+                    </div>
                 </div>
             </Link>
         </div>
